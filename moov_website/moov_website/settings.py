b72b92dbf707fa4ca8fb880e1fbdba8ae648da08
frontedTee <adetunjitayo2019@gmail.com>
2022-07-30 11:51:55 -1200

    modified custom user

--- moov_website/moov_website/settings.py
+++ moov_website/moov_website/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'moov_app'
 ]
 
 MIDDLEWARE = [
@@ -116,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+AUTH_USER_MODEL = 'accounts.NewUser'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
