053ed6d7a25803016c8d71ba70d92f97e6ee3261
kene4500 <42975728+kene4500@users.noreply.github.com>
2022-08-04 11:39:18 +0100

    Revert "create login page"

--- moov_website/accounts/urls.py
+++ moov_website/accounts/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register, name="register"),
-    path('login/', views.loginPage, name="login")
+    path('login/', views.login, name="login")
 ]
--- moov_website/accounts/views.py
+++ moov_website/accounts/views.py
@@ -1,7 +1,4 @@
-from django.shortcuts import render, redirect
-from django.contrib import messages
-from django.contrib.auth import authenticate, login
-from .models import NewUser
+from django.shortcuts import render
 
 # Create your views here.
 
@@ -10,27 +7,5 @@ def register(request):
     return render(request, "accounts/register.html")
 
 
-def loginPage(request):
-    page = 'login'
-
-    if request.user.is_authenticated: #prevents users from entering the login page when already logged in.
-        return redirect('home')
-
-    if request.method == 'POST':
-        email = request.POST.get('email').lower()
-        password = request.POST.get('password')
-
-        try:
-            user = NewUser.objects.get(email=email)
-        except:
-            messages.error(request, 'User does not exist.')
-
-        user = authenticate(request, email=email, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('home')
-        else:
-            messages.error(request, 'Username OR Password does not exist.')
-
-    context = {'page': page}
-    return render(request, 'accounts/login.html', context)
+def login(request):
+    return render(request, 'accounts/login.html')
