8895d204e1ce19390afd2978e2abe94841d52800
frontedTee <adetunjitayo2019@gmail.com>
2022-08-04 13:38:48 -1200

    added codes to deal with case sensitivity

--- moov_website/moov_website/settings.py
+++ moov_website/moov_website/settings.py
@@ -116,6 +116,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+AUTHENTICATION_BACKENDS = {
+    'django.contrib.auth.backends.AllowAllUsersModelBackend',
+    'accounts.backends.CaseInsensitiveModelBackend'
+}
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
