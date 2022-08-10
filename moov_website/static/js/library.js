7d30ce6dbfba85bd7bbd56309fc2b851c7350008
frontedTee <adetunjitayo2019@gmail.com>
2022-08-09 17:45:15 -1200

    Worked on Library interface, added js codes for some animations

--- moov_website/static/js/library.js
+++ moov_website/static/js/library.js
@@ -1,22 +1,33 @@
-function show(){
-  var showContent = document.getElementById("display");
-    showContent.value="<div class='bounceIn'>  \n \n</div>"
- }
-  function show2() {
 
-    var showContent = document.getElementById("display");
-    showContent.value="<div class='BounceInFromBottom'>  \n \n</div>"
-    
-  }
+
+function myFunction() {
+  var showContent = document.getElementById("display");
+  var element = document.getElementById("myDIV");
+  element.classList.toggle("bounce");
+  showContent.value="<div class='BounceIn'>  \n \n</div>"
+  
+}
+function show2() {
+  var showContent = document.getElementById("display");
+  var element = document.getElementById("myDIV");
+  element.classList.toggle("bounceBtm");
+  showContent.value="<div class='BounceInFromBottom'>  \n \n</div>"
+  
+  
+}
+ 
   function show3() {
     var showContent = document.getElementById("display");
-    showContent.value="<div class='BounceOut'>  \n \n</div>"
+  var element = document.getElementById("myDIV");
+  element.classList.toggle("bounceOut");
+  showContent.value="<div class='BounceOut'>  \n \n</div>"
     
   }
   function show4() {
     var showContent = document.getElementById("display");
-    showContent.value="<div class='BounceOutFromBottom'>  \n \n</div>"
-    
+  var element = document.getElementById("myDIV");
+  element.classList.toggle("bounceOutFromBottom");
+  showContent.value="<div class='BounceOutFromBottom'>  \n \n</div>"
   }
   function show5() {
     var showContent = document.getElementById("display");
