7a9c872eeefa8bc9086ba70bd405459ef70da7ba
kene4500 <42975728+kene4500@users.noreply.github.com>
2022-08-11 18:24:57 +0100

    map the links to pages

--- moov_website/moov_app/urls.py
+++ moov_website/moov_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = 'moov_app'
 urlpatterns = [
     path('', views.homepage, name='home'),
     path('error/', views.errorPage, name="error"),
