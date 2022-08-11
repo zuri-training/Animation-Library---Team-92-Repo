8ea1aac4ae5f07b4a4848c577d0648b35e8d00e3
Okafor Patrick <CodesByPatrick>
2022-08-11 14:14:16 +0100

    javascript demo fort docmentation folder

--- moov_website/templates/Documentation folder/documentation.js
+++ moov_website/templates/Documentation folder/documentation.js
@@ -2,7 +2,6 @@ function show1() {
   const Button = document.getElementById("bounce");
   Button.addEventListener("click", function () {
     var Box = document.querySelector(".container1");
-    console.log("remove");
     Box.classList.remove("bounce");
     setTimeout(() => {
       console.log("add");
@@ -14,8 +13,12 @@ function show1() {
 function show2() {
   const Button = document.getElementById("blink");
   Button.addEventListener("click", function () {
-    let Box = document.querySelector(".container2");
-    Box.classList.add("blink").value;
+    var Box = document.querySelector(".container2");
+    Box.classList.remove("blink");
+    setTimeout(() => {
+      console.log("add");
+      Box.classList.add("blink");
+    } , 100);
   });
 }
 
@@ -23,6 +26,10 @@ function show3() {
   const Button = document.getElementById("slideup");
   Button.addEventListener("click", function () {
     var Box = document.querySelector(".container3");
-    Box.classList.toggle("slideUp");
+    Box.classList.remove("slideUp");
+    setTimeout(() => {
+      console.log("add");
+      Box.classList.add("slideUp");
+    } , 100);
   });
 }
