81de0935d56259f125c7f079639bd6d70c36043e
frontedTee <adetunjitayo2019@gmail.com>
2022-08-11 01:51:11 -1200

    added logout views

--- moov_website/accounts/urls.py
+++ moov_website/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-
+from django.contri.auth import views as auth_views
 urlpatterns = [
 
     path('register/', views.register, name='register'),
@@ -8,7 +8,7 @@ urlpatterns = [
     path('success/', views.success, name='success'),
     path('forgot_password/', views.forgot_password, name='forgotPassword'),
     path('contact/', views.contact, name='contact'),
-    path('logout/', views.logout_view, name='logout')
+    path('logout/', views.logout_view, name='logout'),
 
 
 
