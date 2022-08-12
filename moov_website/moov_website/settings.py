824dfdb0a8d01761cada1baa26af43e4de09db3e
frontedTee <adetunjitayo2019@gmail.com>
2022-08-12 04:52:09 -1200

    finished the interface

--- moov_website/moov_website/settings.py
+++ moov_website/moov_website/settings.py
@@ -148,3 +148,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # # Media rootto store images
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+# contact settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'kehindemarthaoluwatoyin@gmail.com'
+EMAIL_HOST_PASSWORD = 'oluwatoyinmartha'
