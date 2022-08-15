2f89c146edd3e952b39890c63e23915420fe43a4
frontedTee <adetunjitayo2019@gmail.com>
2022-08-14 13:42:21 -1200

    updated

--- moov_website/moov_website/settings.py
+++ moov_website/moov_website/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m##ijx5=(u^mrn#!h7jxoe0vi*cu&!bc%cx3wi7qa(t0(0b=8w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
